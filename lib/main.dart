d817bf6de4f85b1d0a59492ec9ee70d4ccffab79
bipulcn <bipulcn@gmail.com>
2022-12-07 19:39:56 +0600

    argument include but not implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
+import 'pgs/pageone.dart';
+import 'pgs/pagetwo.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,92 +11,4 @@ void main() {
       '/second': (context) => const SecondScreen()
     },
   ));
-}
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//           child: ElevatedButton(
-//         onPressed: () {},
-//         child: const Text("Lauch Screen"),
-//       )),
-//     );
-//   }
-// }
-
-class FirsrstScreen extends StatelessWidget {
-  const FirsrstScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("First Screen"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
-          child: const Text("Lauch Screen"),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Second Screen")),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/');
-        },
-        child: const Text('Go Back'),
-      )),
-    );
-  }
 }
--- lib/pgs/pageone.dart
+++ lib/pgs/pageone.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class PageOne extends StatelessWidget {
-  const PageOne({super.key});
+class FirsrstScreen extends StatelessWidget {
+  const FirsrstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second',
+                arguments: "This is first argument from first page");
+          },
+          child: const Text("Lauch Screen"),
+        ),
+      ),
+    );
   }
 }
